fd4a3be650f734586a9d1e6119545faec57fddba
Marcus <60829911+mitigera@users.noreply.github.com>
2024-08-01 15:50:00 +0200

    Create sensitive.py

---
+++ vda/vda/sensitive.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+AWS_access_key_ID = "AKIAIOSFODNN7EXAMPLE"
+AWS_secret_access_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+Google_api_key = "AIzaSyA7q-7a_XvXw1C1xV0ZCEXAMPLEKEY"
+Github_pat = "ghp_1A2B3C4D5E6F7G8H9I0J1K2L3M4N5O6P7Q8R"
+Slack_token = "xoxb-1234-56789abcdefghijklmnop"
+Stripe_secret_key = "sk_test_4eC39HqLyjWDarjtT1zdp7dc"
